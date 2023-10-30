87f43842b9459b931c3af3f0340711acf9edf21d
KrouriB <brice.krouri@gmail.com>
2023-10-30 13:29:46 +0100

    easy admin controller

---
+++ src/Controller/Admin/TypeStatActionCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\TypeStatAction;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class TypeStatActionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return TypeStatAction::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
