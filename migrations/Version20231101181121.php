c5d3cf17d5067859795c8366df85fcb069a0c063
KrouriB <brice.krouri@gmail.com>
2023-11-01 19:15:46 +0100

    migration add rank stat nullable for allow change

---
+++ migrations/Version20231101181121.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231101181121 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE type_stat_action ADD rank_stat1 INT DEFAULT NULL, ADD rank_stat2 INT DEFAULT NULL, ADD rank_stat3 INT DEFAULT NULL, ADD rank_stat4 INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE type_stat_action DROP rank_stat1, DROP rank_stat2, DROP rank_stat3, DROP rank_stat4');
+    }
+}
