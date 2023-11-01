c2403ead52aad7bbb90e1572f2de155308d8be38
KrouriB <brice.krouri@gmail.com>
2023-11-01 22:52:19 +0100

    test here for now

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,17 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Entity\User;
+
+it('should have 9 weapons in stock', function () {
+    $user = new User;
+
+    // dd($user);
+    
+    expect($user->getStockWeapons())->toHaveCount(9);
+});
+
+it('should have 5 perso in stock', function () {
+    $user = new User;
+    
+    expect($user->getStockPersonnages())->toHaveCount(5);
 });
