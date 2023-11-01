b9fa46b915af3b31a11149f9037063356072e70e
KrouriB <brice.krouri@gmail.com>
2023-11-01 22:51:57 +0100

    make i used

--- tests/Units/TestUser.php
+++ tests/Units/TestUser.php
@@ -5,3 +5,9 @@ it('should have 9 weapons in stock', function () {
     
     expect($user->getStockWeapons())->toHaveCount(9);
 });
+
+it('should have 5 perso in stock', function () {
+    $user = new User;
+    
+    expect($user->getStockPersonnages())->toHaveCount(5);
+});
