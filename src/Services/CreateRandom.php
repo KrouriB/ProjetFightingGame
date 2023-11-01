b2839cfbc1a066a238f186982b6a4fb8b11a60a7
KrouriB <brice.krouri@gmail.com>
2023-11-01 22:34:22 +0100

    service to create rand value to use as id selector

---
+++ src/Services/CreateRandom.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Ad;
+
+class CreateRandom
+{
+    public function createFiveRandom(): array
+    {
+        $randOne = rand(1, 45);
+        $randTwo = rand(1, 45);
+        while($randOne == $randTwo)
+        {
+            $randTwo = rand(1, 45);
+        }
+        $randThree = rand(1, 45);
+        while($randOne == $randThree || $randTwo == $randThree)
+        {
+            $randThree = rand(1, 45);
+        }
+        $randFour = rand(1, 45);
+        while($randOne == $randFour || $randTwo == $randFour || $randThree == $randFour)
+        {
+            $randFour = rand(1, 45);
+        }
+        $randFive = rand(1, 45);
+        while($randOne == $randFive || $randTwo == $randFive || $randThree == $randFive || $randFour == $randFive)
+        {
+            $randFive = rand(1, 45);
+        }
+
+        $random = [$randOne, $randTwo, $randThree, $randFour, $randFive];
+
+        return $random;
+    }
+}
