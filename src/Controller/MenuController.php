3e697b31d48f88d85292afa511e3ba682d3edbcf
KrouriB <brice.krouri@gmail.com>
2023-11-03 16:44:18 +0100

    import UserRepository

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use App\Repository\EquipeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MenuController extends AbstractController
 {
