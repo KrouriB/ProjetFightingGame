bd97b1e42827b9fabb7b858cf640a36e172fe9e4
KrouriB <brice.krouri@gmail.com>
2023-11-06 14:14:51 +0100

    form for teammate

--- src/Form/EquipeType.php
+++ src/Form/EquipeType.php
@@ -3,15 +3,32 @@
 namespace App\Form;
 
 use App\Entity\Equipe;
+use App\Entity\Weapon;
+use App\Entity\Accessory;
+use App\Entity\Personnage;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class EquipeType extends AbstractType
 {
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // dd($this->security->getUser());
         $builder
             ->add(
                 'teamName',
@@ -23,19 +40,68 @@ class EquipeType extends AbstractType
                 ]
             )
             ->add(
-                'assosiatedUser',
+                'personnage',
                 EntityType::class,
                 [
-                    'class' => Formation::class,
-                    'choice_label' => 'nom',
+                    'class' => Personnage::class,
+                    'query_builder' => function (EntityRepository $er): QueryBuilder {
+                        return $er->createQueryBuilder('p')
+                            ->InnerJoin('p.stockPersonnages', 'sp')
+                            ->InnerJoin('sp.stockUser', 'u')
+                            ->andWhere('u.id = :id')
+                            ->setParameter('id', $this->security->getUser()->getId());
+                    },
+                    'choice_label' => 'name',
                     'attr' => [
                         'class' => 'form-control'
                     ]
                 ]
             )
-            ->add('personnage')
-            ->add('weapon')
-            ->add('accessory')
+            ->add(
+                'weapon',
+                EntityType::class,
+                [
+                    'class' => Weapon::class,
+                    'query_builder' => function (EntityRepository $er): QueryBuilder {
+                        return $er->createQueryBuilder('w')
+                            ->InnerJoin('w.stockWeapons', 'sw')
+                            ->InnerJoin('sw.stockUser', 'u')
+                            ->andWhere('u.id = :id')
+                            ->setParameter('id', $this->security->getUser()->getId());
+                    },
+                    'choice_label' => 'name',
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'accessory',
+                EntityType::class,
+                [
+                    'class' => Accessory::class,
+                    'query_builder' => function (EntityRepository $er): QueryBuilder {
+                        return $er->createQueryBuilder('a')
+                            ->InnerJoin('a.stockAccessorys','sa')
+                            ->InnerJoin('sa.stockUser', 'u')
+                            ->andWhere('u.id = :id')
+                            ->setParameter('id', $this->security->getUser()->getId());
+                    },
+                    'choice_label' => 'name',
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+                ]
+            )
+            ->add(
+                'valider',
+                SubmitType::class,
+                [
+                    'attr' => [
+                        'class' => 'btn btn-success'
+                    ]
+                ]
+            )
         ;
     }
 
