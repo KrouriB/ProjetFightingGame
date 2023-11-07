f0e8cacf50deb5143ec9001e124d76e0fe4aad9c
KrouriB <brice.krouri@gmail.com>
2023-11-07 11:47:17 +0100

    Regex

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -40,10 +40,14 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 12,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[A-Za-z0-9$%#@!]{8,}$/',
+                        'message' => 'The password must meet the regex pattern: {{ pattern }}',
                     ]),
                 ],
                 'first_options'  => ['label' => 'Password'],
