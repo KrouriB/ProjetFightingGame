47e21c61040f57b23b74d883b4d889858b5ece70
KrouriB <brice.krouri@gmail.com>
2023-11-09 10:33:09 +0100

    modified + add new method

--- src/Services/CreateRandom.php
+++ src/Services/CreateRandom.php
@@ -8,30 +8,35 @@ class CreateRandom
 {
     public function createFiveRandom(): array
     {
-        $randOne = rand(1, 45);
-        $randTwo = rand(1, 45);
+        $randOne = rand(0, 44);
+        $randTwo = rand(0, 44);
         while($randOne == $randTwo)
         {
-            $randTwo = rand(1, 45);
+            $randTwo = rand(0, 44);
         }
-        $randThree = rand(1, 45);
+        $randThree = rand(0, 44);
         while($randOne == $randThree || $randTwo == $randThree)
         {
-            $randThree = rand(1, 45);
+            $randThree = rand(0, 44);
         }
-        $randFour = rand(1, 45);
+        $randFour = rand(0, 44);
         while($randOne == $randFour || $randTwo == $randFour || $randThree == $randFour)
         {
-            $randFour = rand(1, 45);
+            $randFour = rand(0, 44);
         }
-        $randFive = rand(1, 45);
+        $randFive = rand(0, 44);
         while($randOne == $randFive || $randTwo == $randFive || $randThree == $randFive || $randFour == $randFive)
         {
-            $randFive = rand(1, 45);
+            $randFive = rand(0, 44);
         }
 
         $random = [$randOne, $randTwo, $randThree, $randFour, $randFive];
 
         return $random;
+    }
+
+    public function createSingleRandom(int $lenght): int
+    {
+        return rand(0, $lenght);
     }
 }
