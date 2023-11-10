49b6c5798febaa4e143a79304fe71eae44bf081e
KrouriB <brice.krouri@gmail.com>
2023-11-10 17:00:47 +0100

    const session + start of func

--- public/js/fight.js
+++ public/js/fight.js
@@ -1,2 +1,9 @@
 var ennemie = sessionStorage.getItem("ennemie");
 var ally = sessionStorage.getItem("ally");
+const ennemieData = sessionStorage.getItem("ennemieData");
+const allyData = sessionStorage.getItem("allyData");
+
+function allyAttack()
+{
+    console.log(allyData);
+}
