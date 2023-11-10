19d3dc0bb02075bed95234825f772824b8e0adb0
KrouriB <brice.krouri@gmail.com>
2023-11-10 16:51:32 +0100

    correction du fichier

--- src/Services/JsonBuilder.php
+++ src/Services/JsonBuilder.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace App\Service;
+namespace App\Services;
 
-use App\Entity\Ad;
+use App\Entity\Equipe;
 
 class JsonBuilder
 {
     public function stockData(Equipe $equipe)
     {
         $personnage = $equipe->getPersonnage();
-        $weapon = getWeapon();
-        $accessory = getAccessory();
+        $weapon = $equipe->getWeapon();
+        $accessory = $equipe->getAccessory();
         switch($accessory->getStatAction())
         {
             case 1:
@@ -27,12 +27,14 @@ class JsonBuilder
                 break;
         }
         $data = [];
-        $data['personnage'] = ['Element' => $personnage->getElement()->getNameElement(), 'Type' => $personnage->getTypeWeapon()->getNameType(), 'Category' => $personnage->getCategoryWeapon()->getNameCategory(), 'Attack' => $personnage->getAttack(), 'Magic' => $personnage->getMagic(), 'Life' => $personnage->getLife(),  'Energy' => $personnage->getEnergy()];
-        $data['weapon'] = ['ElementBase' => $weapon->getWeaponElement()->getNameElement(), 'ElementSkill' => $weapon->getSkillElement()->getNameElement(), 'Type' => $personnage->getTypeWeapon()->getNameType(), 'Category' => $weapon->getCategoryWeapon()->getNameCategory(), 'baseAttack' => $weapon->getAttackStat(), 'baseMagic' => $weapon->getMagicStat(), 'skillAttack' => $weapon->getAttackSkill(), 'skillMagic' => $weapon->getMagicSkill(), 'EnergyCost' => $weapon->getEnergySkill(), 'turnWait' => $weapon->getWaitSkill(), 'skillName' => $weapon->getNameSkill()];
+        $data['personnage'] = ['Element' => $personnage->getElement()->getNameElement(), 'Type' => $personnage->getType()->getNameType(), 'Category' => $personnage->getCategory()->getNameCategory(), 'Attack' => $personnage->getAttack(), 'Magic' => $personnage->getMagic(), 'Life' => $personnage->getLife(),  'Energy' => $personnage->getEnergy()];
+        $data['weapon'] = ['ElementBase' => $weapon->getWeaponElement()->getNameElement(), 'ElementSkill' => $weapon->getSkillElement()->getNameElement(), 'Type' => $weapon->getWeaponType()->getNameType(), 'Category' => $weapon->getWeaponCategory()->getNameCategory(), 'baseAttack' => $weapon->getAttackStat(), 'baseMagic' => $weapon->getMagicStat(), 'skillAttack' => $weapon->getAttackSkill(), 'skillMagic' => $weapon->getMagicSkill(), 'EnergyCost' => $weapon->getEnergySkill(), 'turnWait' => $weapon->getWaitSkill(), 'skillName' => $weapon->getNameSkill()];
         $data['accessory'] = ['NomStatAction' => $accessory->getTypeStatAction()->getNomStat(), 'NomTypeAction' => $accessory->getTypeStatAction()->getNomType(), 'StatAction' => $action, 'Defense' => $accessory->getDefense(), 'Resistance' => $accessory->getResistance(), 'Attack' => $accessory->getBonusAttack(), 'Magic' => $accessory->getBonusMagic(), 'Recovery' => $accessory->getEnergyRecovery(), 'turnWait' => $accessory->getWaitAction()];
         // for personnage we send life & energy data here too because of accessory action that can be dependent from this stat
 
         $jsonFile = json_encode($data);
+
+        // dd($jsonFile);
 
         return $jsonFile;
     }
