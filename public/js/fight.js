05e4ed9dc06b406f6a3d2bd1eb51e5ca76110122
KrouriB <brice.krouri@gmail.com>
2023-11-13 12:01:32 +0100

    start of methode to check element

--- public/js/fight.js
+++ public/js/fight.js
@@ -1,9 +1,28 @@
-var ennemie = sessionStorage.getItem("ennemie");
-var ally = sessionStorage.getItem("ally");
-const ennemieData = sessionStorage.getItem("ennemieData");
-const allyData = sessionStorage.getItem("allyData");
+const ennemieData = JSON.parse(ennemie.replaceAll('&quot;', '"'));
+const allyData = JSON.parse(ally.replaceAll('&quot;', '"'));
+var ennemieInfo = JSON.parse(ennemieInfo.replaceAll('&quot;', '"'));
+var allyInfo = JSON.parse(allyInfo.replaceAll('&quot;', '"'));
 
 function allyAttack()
 {
+
+    // console.log(ennemieData);
     console.log(allyData);
+    // console.log(ennemieInfo);
+    // console.log(allyInfo);
+    // console.log(ennemie.replaceAll('&quot;', '"'));
+}
+
+function checkActionElement(action)
+{
+    if(action == base)
+    {
+
+    }
+    else if(action == skill)
+    {
+
+    }
+    else
+    console.log(allyData.personnage.Element == allyData.weapon.ElementBase)
 }
