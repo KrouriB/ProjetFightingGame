58cd51fedb77a5c0e483f335ab33578a5396bd18
KrouriB <brice.krouri@gmail.com>
2023-11-13 12:00:27 +0100

    more methods to create adapted json strings

--- src/Services/JsonBuilder.php
+++ src/Services/JsonBuilder.php
@@ -3,9 +3,32 @@
 namespace App\Services;
 
 use App\Entity\Equipe;
+use App\Repository\ElementRepository;
+use App\Repository\TypeWeaponRepository;
+use App\Repository\CategoryWeaponRepository;
 
 class JsonBuilder
 {
+    private ElementRepository $elementRepository;
+    private TypeWeaponRepository $typeWeaponRepository;
+    private CategoryWeaponRepository $categoryWeaponRepository;
+
+    public function __construct(ElementRepository $elementRepository, TypeWeaponRepository $typeWeaponRepository, CategoryWeaponRepository $categoryWeaponRepository)
+    {
+        $this->elementRepository = $elementRepository;
+        $this->typeWeaponRepository = $typeWeaponRepository;
+        $this->categoryWeaponRepository = $categoryWeaponRepository;
+    }
+
+    public function setInfo(Equipe $equipe)
+    {
+        $data = ['hp' => $equipe->getPersonnage()->getLife(), 'energy' => $equipe->getPersonnage()->getEnergy()];
+        
+        $jsonFile = json_encode($data);
+
+        return $jsonFile;
+    }
+
     public function stockData(Equipe $equipe)
     {
         $personnage = $equipe->getPersonnage();
@@ -26,15 +49,63 @@ class JsonBuilder
                 $action = $accessory->getTypeStatAction()->getRankStat4();
                 break;
         }
-        $data = [];
-        $data['personnage'] = ['Element' => $personnage->getElement()->getNameElement(), 'Type' => $personnage->getType()->getNameType(), 'Category' => $personnage->getCategory()->getNameCategory(), 'Attack' => $personnage->getAttack(), 'Magic' => $personnage->getMagic(), 'Life' => $personnage->getLife(),  'Energy' => $personnage->getEnergy()];
-        $data['weapon'] = ['ElementBase' => $weapon->getWeaponElement()->getNameElement(), 'ElementSkill' => $weapon->getSkillElement()->getNameElement(), 'Type' => $weapon->getWeaponType()->getNameType(), 'Category' => $weapon->getWeaponCategory()->getNameCategory(), 'baseAttack' => $weapon->getAttackStat(), 'baseMagic' => $weapon->getMagicStat(), 'skillAttack' => $weapon->getAttackSkill(), 'skillMagic' => $weapon->getMagicSkill(), 'EnergyCost' => $weapon->getEnergySkill(), 'turnWait' => $weapon->getWaitSkill(), 'skillName' => $weapon->getNameSkill()];
-        $data['accessory'] = ['NomStatAction' => $accessory->getTypeStatAction()->getNomStat(), 'NomTypeAction' => $accessory->getTypeStatAction()->getNomType(), 'StatAction' => $action, 'Defense' => $accessory->getDefense(), 'Resistance' => $accessory->getResistance(), 'Attack' => $accessory->getBonusAttack(), 'Magic' => $accessory->getBonusMagic(), 'Recovery' => $accessory->getEnergyRecovery(), 'turnWait' => $accessory->getWaitAction()];
+        $personnage = ['Element' => $personnage->getElement()->getNameElement(), 'Type' => $personnage->getType()->getNameType(), 'Category' => $personnage->getCategory()->getNameCategory(), 'Attack' => $personnage->getAttack(), 'Magic' => $personnage->getMagic(), 'Life' => $personnage->getLife(),  'Energy' => $personnage->getEnergy()];
+        $weapon = ['ElementBase' => $weapon->getWeaponElement()->getNameElement(), 'ElementSkill' => $weapon->getSkillElement()->getNameElement(), 'Type' => $weapon->getWeaponType()->getNameType(), 'Category' => $weapon->getWeaponCategory()->getNameCategory(), 'baseAttack' => $weapon->getAttackStat(), 'baseMagic' => $weapon->getMagicStat(), 'skillAttack' => $weapon->getAttackSkill(), 'skillMagic' => $weapon->getMagicSkill(), 'EnergyCost' => $weapon->getEnergySkill(), 'turnWait' => $weapon->getWaitSkill(), 'skillName' => $weapon->getNameSkill()];
+        $accessory = ['NomStatAction' => $accessory->getTypeStatAction()->getNomStat(), 'NomTypeAction' => $accessory->getTypeStatAction()->getNomType(), 'StatAction' => $action, 'Defense' => $accessory->getDefense(), 'Resistance' => $accessory->getResistance(), 'Attack' => $accessory->getBonusAttack(), 'Magic' => $accessory->getBonusMagic(), 'Recovery' => $accessory->getEnergyRecovery(), 'turnWait' => $accessory->getWaitAction()];
+        $data = ['personnage' => $personnage, 'weapon' => $weapon, 'accessory' => $accessory];
         // for personnage we send life & energy data here too because of accessory action that can be dependent from this stat
 
         $jsonFile = json_encode($data);
 
         // dd($jsonFile);
+
+        return $jsonFile;
+    }
+
+    public function elementData()
+    {
+        $elements = $this->elementRepository->findAll();
+
+        $data = [];
+
+        foreach($elements as $element)
+        {
+            $data[] = ['id' => $element->getId(), 'name' => $element->getNameElment(), 'advantage' => $this->elementRepository->find($element->getAdvantage()), 'advantageMultiplicator' => $element->getAdvantageMultiplicator(), 'disadvantage' => $this->elementRepository->find($element->getDisadvantage()), 'disadvantageMultiplicator' => $element->getDisadvantageMultiplicator()];
+        }
+        
+        $jsonFile = json_encode($data);
+
+        return $jsonFile;
+    }
+
+    public function typeData()
+    {
+        $types = $this->typeWeaponRepository->findAll();
+
+        $data = [];
+
+        foreach($types as $type)
+        {
+            $data[] = ['id' => $type->getId(), 'name' => $type->getNameType(), 'advantage' => $this->typeWeaponRepository->find($type->getAdvantage()), 'advantageMultiplicator' => $type->getAdvantageMultiplicator(), 'disadvantage' => $this->typeWeaponRepository->find($type->getDisadvantage()), 'disadvantageMultiplicator' => $type->getDisadvantageMultiplicator()];
+        }
+        
+        $jsonFile = json_encode($data);
+
+        return $jsonFile;
+    }
+
+    public function categoryData()
+    {
+        $types = $this->categoryWeaponRepository->findAll();
+
+        $data = [];
+
+        foreach($categorys as $category)
+        {
+            $data[] = ['id' => $category->getId(), 'name' => $category->getNameCategory(), 'advantage' => $this->categoryWeaponRepository->find($category->getAdvantage()), 'advantageMultiplicator' => $category->getAdvantageMultiplicator(), 'disadvantage' => $this->categoryWeaponRepository->find($category->getDisadvantage()), 'disadvantageMultiplicator' => $category->getDisadvantageMultiplicator()];
+        }
+        
+        $jsonFile = json_encode($data);
 
         return $jsonFile;
     }
