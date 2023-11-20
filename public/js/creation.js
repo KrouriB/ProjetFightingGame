31d2bd66f40d1497ef15ff99478fb6d1804ce427
KrouriB <brice.krouri@gmail.com>
2023-11-20 14:36:43 +0100

    made creation personnage

--- public/js/creation.js
+++ public/js/creation.js
@@ -3,62 +3,282 @@
 
 
 // constant and variable
+var pointsPersonnage = 31;
 const hpPerso = document.getElementById("personnage_life");
+const atkPerso = document.getElementById("personnage_attack");
+const magPerso = document.getElementById("personnage_magic");
+const nrjPerso = document.getElementById("personnage_energy");
 
 
 // calculate point
-function showPoint()
+function showPointsPersonnage(pts)
 {
-
+    let spanPts = document.getElementById("pointAllouable");
+    spanPts.innerText = pts + '/31';
 }
 
 
 // function more
 
+
 function moreHpPerso()
 {
     let button = document.getElementById('hpPlus');
+
+    if(document.getElementById('hpMinus').classList.contains('grey'))
+    {
+        document.getElementById('hpMinus').classList.remove('grey');
+    }
+
     let value = parseInt(hpPerso.value, 10);
     value = isNaN(value) ? 200 : value;
-    if(value < 500)
+    if(value < 500 && pointsPersonnage > 0)
     {
         value = value + 25;
+        showPointsPersonnage(--pointsPersonnage);
     }
 
     hpPerso.value = value;
 
     if(value == 500)
     {
-        button.classList.add(grey);
+        button.classList.add('grey');
     }
-    else if(button.classList.contains(grey))
+    else if(button.classList.contains('grey'))
     {
-        button.disabled.remove(grey);
+        button.classList.remove('grey');
+    }
+}
+
+function moreAtkPerso()
+{
+    let button = document.getElementById('atkPlus');
+
+    if(document.getElementById('atkMinus').classList.contains('grey'))
+    {
+        document.getElementById('atkMinus').classList.remove('grey');
+    }
+
+    let value = parseInt(atkPerso.value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value < 40 && pointsPersonnage > 0)
+    {
+        value = value + 4;
+        showPointsPersonnage(--pointsPersonnage);
+    }
+
+    atkPerso.value = value;
+
+    if(value == 40)
+    {
+        button.classList.add('grey');
+    }
+    else if(button.classList.contains('grey'))
+    {
+        button.classList.remove('grey');
+    }
+}
+
+function moreMagPerso()
+{
+    let button = document.getElementById('magPlus');
+
+    if(document.getElementById('magMinus').classList.contains('grey'))
+    {
+        document.getElementById('magMinus').classList.remove('grey');
+    }
+
+    let value = parseInt(magPerso.value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value < 40 && pointsPersonnage > 0)
+    {
+        value = value + 4;
+        showPointsPersonnage(--pointsPersonnage);
+    }
+
+    magPerso.value = value;
+
+    if(value == 40)
+    {
+        button.classList.add('grey');
+    }
+    else if(button.classList.contains('grey'))
+    {
+        button.classList.remove('grey');
+    }
+}
+
+function moreNrjPerso()
+{
+    let button = document.getElementById('nrjPlus');
+
+    if(document.getElementById('nrjMinus').classList.contains('grey'))
+    {
+        document.getElementById('nrjMinus').classList.remove('grey');
+    }
+
+    let value = parseInt(nrjPerso.value, 10);
+    value = isNaN(value) ? 60 : value;
+    if(value < 180 && pointsPersonnage > 0)
+    {
+        value = value + 8;
+        showPointsPersonnage(--pointsPersonnage);
+    }
+
+    nrjPerso.value = value;
+
+    if(value == 180)
+    {
+        button.classList.add('grey');
+    }
+    else if(button.classList.contains('grey'))
+    {
+        button.classList.remove('grey');
     }
 }
 
 
 // function less
 
+
 function lessHpPerso()
 {
     let button = document.getElementById('hpMinus');
+
+    if(document.getElementById('hpPlus').classList.contains('grey'))
+    {
+        document.getElementById('hpPlus').classList.remove('grey');
+    }
+
     var value = parseInt(hpPerso.value, 10);
     value = isNaN(value) ? 200 : value;
 
     if(value > 200)
     {
         value = value - 25;
+        showPointsPersonnage(++pointsPersonnage);
     }
 
     hpPerso.value = value;
 
     if(value == 200)
     {
-        button.classList.add(grey);
+        button.classList.add('grey');
     }
-    else if(button.classList.contains(grey))
+    else if(button.classList.contains('grey'))
     {
-        button.disabled.remove(grey);
+        button.classList.remove('grey');
     }
 }
+
+function lessAtkPerso()
+{
+    let button = document.getElementById('atkMinus');
+
+    if(document.getElementById('atkPlus').classList.contains('grey'))
+    {
+        document.getElementById('atkPlus').classList.remove('grey');
+    }
+
+    var value = parseInt(atkPerso.value, 10);
+    value = isNaN(value) ? 0 : value;
+
+    if(value > 0)
+    {
+        value = value - 4;
+        showPointsPersonnage(++pointsPersonnage);
+    }
+
+    atkPerso.value = value;
+
+    if(value == 0)
+    {
+        button.classList.add('grey');
+    }
+    else if(button.classList.contains('grey'))
+    {
+        button.classList.remove('grey');
+    }
+}
+
+function lessMagPerso()
+{
+    let button = document.getElementById('magMinus');
+
+    if(document.getElementById('magPlus').classList.contains('grey'))
+    {
+        document.getElementById('magPlus').classList.remove('grey');
+    }
+
+    var value = parseInt(magPerso.value, 10);
+    value = isNaN(value) ? 0 : value;
+
+    if(value > 0)
+    {
+        value = value - 4;
+        showPointsPersonnage(++pointsPersonnage);
+    }
+
+    magPerso.value = value;
+
+    if(value == 0)
+    {
+        button.classList.add('grey');
+    }
+    else if(button.classList.contains('grey'))
+    {
+        button.classList.remove('grey');
+    }
+}
+
+function lessNrjPerso()
+{
+    let button = document.getElementById('nrjMinus');
+
+    if(document.getElementById('nrjPlus').classList.contains('grey'))
+    {
+        document.getElementById('nrjPlus').classList.remove('grey');
+    }
+
+    let value = parseInt(nrjPerso.value, 10);
+    value = isNaN(value) ? 60 : value;
+    if(value > 60)
+    {
+        value = value - 8;
+        showPointsPersonnage(++pointsPersonnage);
+    }
+
+    nrjPerso.value = value;
+
+    if(value == 60)
+    {
+        button.classList.add('grey');
+    }
+    else if(button.classList.contains('grey'))
+    {
+        button.classList.remove('grey');
+    }
+}
+
+
+// when validate
+
+
+async function makeNotDisable(stat){
+    hpPerso.disabled = stat;
+    atkPerso.disabled = stat;
+    magPerso.disabled = stat;
+    nrjPerso.disabled = stat;
+}
+
+async function validate()
+{
+    await makeNotDisable(false);
+    document.getElementById("personnage_valider").click();
+}
+
+
+// initialize
+
+showPointsPersonnage(pointsPersonnage);
+makeNotDisable(true);
