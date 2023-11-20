551511ce203c77adc292aeb04b38c730c2bff99a
KrouriB <brice.krouri@gmail.com>
2023-11-20 11:36:36 +0100

    add delete class when reached max/min stat

--- public/js/creation.js
+++ public/js/creation.js
@@ -6,18 +6,35 @@
 const hpPerso = document.getElementById("personnage_life");
 
 
+// calculate point
+function showPoint()
+{
+
+}
+
+
 // function more
 
 function moreHpPerso()
 {
+    let button = document.getElementById('hpPlus');
     let value = parseInt(hpPerso.value, 10);
     value = isNaN(value) ? 200 : value;
     if(value < 500)
     {
-        value = value + 100;
+        value = value + 25;
     }
 
     hpPerso.value = value;
+
+    if(value == 500)
+    {
+        button.classList.add(grey);
+    }
+    else if(button.classList.contains(grey))
+    {
+        button.disabled.remove(grey);
+    }
 }
 
 
@@ -25,13 +42,23 @@ function moreHpPerso()
 
 function lessHpPerso()
 {
+    let button = document.getElementById('hpMinus');
     var value = parseInt(hpPerso.value, 10);
     value = isNaN(value) ? 200 : value;
 
     if(value > 200)
     {
-        value = value - 100;
+        value = value - 25;
     }
 
     hpPerso.value = value;
+
+    if(value == 200)
+    {
+        button.classList.add(grey);
+    }
+    else if(button.classList.contains(grey))
+    {
+        button.disabled.remove(grey);
+    }
 }
