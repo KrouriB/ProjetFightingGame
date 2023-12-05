31a4436f355fbab841621a90b0fa60939a544cbb
KrouriB <brice.krouri@gmail.com>
2023-12-06 00:14:07 +0100

    more info

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -20,10 +21,11 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class)
             ->add('email', EmailType::class)
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'J\'accepte les conditions générales',
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -42,7 +44,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Regex([
                         'pattern' => "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?! @$%^&* -]).{12,}$/",
-                        'message' => 'The password must meet the regex pattern: {{ pattern }}',
+                        'message' => 'Le mot de passe doit avoir une longueur de 12 caractères, 1 Majuscule, 1 Miniscule, 1 Chiffre et 1 Caractère spécial',
                     ]),
                 ],
                 'first_options'  => ['label' => 'Password'],
