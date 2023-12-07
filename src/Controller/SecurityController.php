d10ee45a1fa86064f4095d0645eb4b3a57211d35
KrouriB <brice.krouri@gmail.com>
2023-12-08 00:14:17 +0100

    new method

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,17 @@
 namespace App\Controller;
 
 use App\Form\UserType;
+use App\Form\NewPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class SecurityController extends AbstractController
 {
@@ -27,27 +33,64 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/user', name: 'app_user')]
-    public function user(EntityManagerInterface $entityManager): Response
+    public function user(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserInterface $userInterface): Response
     {
-        $form = $this->createForm(UserType::class, $this->getUser());
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+            $enteredPassword = $form->get('password')->getData();
+            if (!$userPasswordHasher->isPasswordValid($userInterface, $enteredPassword))
+            {
+                $this->addFlash('error', 'Mot de passe invalde.');
+
+                return $this->redirectToRoute('app_user');
+            }
+
+            $this->addFlash('success', 'Les information on été modifier.');
+
             $entityManager->flush();
         }
 
         return $this->render('security/user.html.twig', [
             'userInfo' => $form->createView(),
         ]);
+    }
 
+    #[Route(path: '/password', name: 'app_password')]
+    public function newPassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserInterface $userInterface)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(NewPasswordType::class, $user);
+        $form->handleRequest($request);
 
-        return $this->render('security/user.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $enteredPassword = $form->get('passwordConfirm')->getData();
+            if (!$userPasswordHasher->isPasswordValid($userInterface, $enteredPassword))
+            {
+                $this->addFlash('error', 'Mot de passe invalde.');
+
+                return $this->redirectToRoute('app_user');
+            }
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            
+            $entityManager->flush();
+            
+            $this->addFlash('success', 'Le mot de passe a été modifier.');
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('security/password.html.twig', [
+            'userInfo' => $form->createView(),
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
@@ -55,4 +98,18 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/deleteUser', name: 'app_delete_user')]
+    public function deleteCurrentUser(SessionInterface $session, EntityManagerInterface $entityManager)
+    {
+        $user = $this->getUser();
+        $session->invalidate();
+        $session->migrate(true); // crete new session to prevent session fixation attacks
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_not_co');
+    }
+
+
 }
