3da83fd8d15eb42c7b049b9de451405bf030cbf9
KrouriB <brice.krouri@gmail.com>
2023-12-13 04:38:54 +0100

    carrousel

--- public/js/script.js
+++ public/js/script.js
@@ -64,3 +64,46 @@ $(document).ready(function() {
         }
     })
 })
+
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+// Images are from unsplash
+let pictures = [
+    "/img/character/axesman.webp",
+    "/img/character/swordsman.webp",
+    "/img/character/spearsman.webp",
+    "/img/character/mage_tattou_midJourney_modified.webp",
+    "/img/character/mage_book_midjourney_modified.webp",
+    "/img/character/mageWand.webp",
+    "/img/character/fighter.webp",
+    "/img/character/thief.webp",
+    "/img/character/ranger.webp",
+]
+
+img.src = pictures[0];
+let position = 0;
+
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
